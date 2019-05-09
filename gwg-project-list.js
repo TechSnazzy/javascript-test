0cff3065f35e9dbbf7b6deae367760caa69d8e24
Sean Morrison <sean1972@gmail.com>
2019-05-08 22:11:00 -0700

    Added the Grow With Google completed projects list

---
+++ gwg-project-list.js
@@ -0,0 +1,37 @@
+var projects = {
+  map: {
+    name: 'Neighborhood Map (React)',
+    complete: '10/17/18',
+    due: '11/10/18'
+  },
+  myreads: {
+    name: 'My Reads: A Book Tracking App',
+    complete: '09/24/18',
+    due: '10/13/18'
+  },
+  reviews: {
+    name: 'Restaurant Reviews App - Stage 1',
+    complete: '09/03/18',
+    due: '09/18/18'
+  },
+  feed: {
+    name: 'Feed Reader Testing',
+    complete: '07/23/18',
+    due: '07/30/18'
+  },
+  arcade: {
+    name: 'Classic Arcade Game Clone',
+    complete: '07/09/18',
+    due: '07/16/18'
+  },
+  memory: {
+    name: 'Memory Game',
+    complete: '06/04/18',
+    due: '06/04/18'
+  },
+  portfolio: {
+    name: 'Build a Portfolio Site',
+    complete: '05/10/18',
+    due: '05/14/18'
+  }
+};
