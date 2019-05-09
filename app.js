4180181b01957e09821fef6362c690503d039456
Sean Morrison <sean1972@gmail.com>
2019-05-08 22:26:16 -0700

    Added a fruit array and printed a string to the console using template literals

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ let code = {
   }
 };
 
+let fruit = [
+  'Apple',
+  'Orange',
+  function() {
+    return 'Banana';
+  }
+];
+
 console.log(
   '#' + code.todo(100) + 'DaysOfCode:' + ' Continue learning ' + code.verb()
 );
+
+console.log(`I have an ${fruit[0]} and a ${fruit[1]} and a ${fruit[2]()}.`);
