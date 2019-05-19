0c948276364ce603c562ed558428ea432cc23cd9
Sean Morrison <sean1972@gmail.com>
2019-05-19 12:59:16 -0700

    Example of constructor

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
-function Apple(x, y, color, score) {
-  this.x = x;
-  this.y = y;
+/*
+What is happening here?
+1. Create a new Apple constructor object.
+2. Create a new Apple prototype object.
+3. Create new apple1, apple2, apple3 objects.
+4. Print results to the console.
+*/
+
+function Apple(color, weight) {
   this.color = color;
-  this.score = score;
+  this.weight = weight;
 }
 
-let apple1 = new Apple(10, 20, 'Red', 200);
-let apple2 = new Apple(100, 200, 'Green', 50);
-let apple3 = new Apple(20, 200, 'Pink', 10);
+Apple.prototype = {
+  eat: function() {
+    return 'eat the apple';
+  },
+  throw: function() {
+    return 'throw the apple';
+  }
+};
 
-console.log(apple1);
-console.log(apple2);
-console.log(apple3);
+let apple1 = new Apple('red', 99);
+let apple2 = new Apple('green', 109);
+let apple3 = new Apple('yellow', 299);
+
+console.log(
+  'My apple is ' +
+    apple1.color +
+    ' and weighs ' +
+    apple1.weight +
+    ' ounces and I will ' +
+    apple1.eat() +
+    '.'
+);
