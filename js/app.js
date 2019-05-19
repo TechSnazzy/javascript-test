cc5fa88985d4d5ebf51f411359ba37ae10b56c91
Sean Morrison <sean1972@gmail.com>
2019-05-19 10:14:49 -0700

    Create a good This example showing how This keyword works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 /*
-1. This references the object that is executing the current function.
-2. If the function is part of an object, we call it a method.
-3. If the function is a method in an object, this references the object itself.
-3. If the function is a regular function which means it's not part of an object then this references the global object which is the window object in browsers.
+This is how 'this' keyword works.
+1. Create a person object.
+2. Add a firstName and lastName property.
+3. Add a getFullName method which returns the person first and last name.
+4. Now call the person's full name and assign it to a variable.
+5. Print the fullName variable to the console.
+6. Create a new person2 variable and assign it to person. Notice how it returns undefined to the console.
+8. Change person.firstName to this.firstName so that the object symbol no longer matters.
 */
+
+let person = {
+  firstName: 'Sean',
+  lastName: 'Morrison',
+  getFullName: function() {
+    return this.firstName + ' ' + this.lastName;
+  }
+};
+
+let fullName = person.getFullName();
+
+let person2 = person;
+person = {};
+
+console.log(fullName);
