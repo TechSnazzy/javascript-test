94871853ec7a2cc2d4d82dc870240d18ced71ebd
Sean Morrison <sean1972@gmail.com>
2019-05-19 22:19:26 -0700

    Example of three types of For loops

--- js/app.js
+++ js/app.js
@@ -11,3 +11,9 @@ for (let i = 0; i < classRegister.length; i++) {
 for (let i = classRegister.length - 1; i >= 0; i--) {
   console.log(classRegister[i]);
 }
+
+// The for...in loop
+
+for (var index in classRegister) {
+  console.log(classRegister[index]);
+}
