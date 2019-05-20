a0c81b9e7c4f06a3ef276e49e83f8140c59b7531
Sean Morrison <sean1972@gmail.com>
2019-05-19 21:57:37 -0700

    Updated conditional If example

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-var carsLeft = 1,
+let carsLeft = 1,
   carsRight = 0,
   greenMan = 'no';
 
 if (greenMan === 'yes') {
   console.log('Cross the road');
-} else if (carsLeft === 0) {
+} else if (greenMan === 'yes' && (carsLeft === 0 || carsRight === 0)) {
   console.log('All clear: Cross the road');
 } else {
   console.log('Stay where you are.');
