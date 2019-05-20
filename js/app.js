c4aa5f64568d03cf6e23cd741ede2f1e8c1c50c7
Sean Morrison <sean1972@gmail.com>
2019-05-19 21:47:45 -0700

    Example of conditional If statement

--- js/app.js
+++ js/app.js
@@ -1,24 +1,11 @@
-function Cat(color, gender) {
-  this.color = color;
-  this.gender = gender;
+var carsLeft = 1,
+  carsRight = 0,
+  greenMan = 'no';
+
+if (greenMan === 'yes') {
+  console.log('Cross the road');
+} else if (carsLeft === 0) {
+  console.log('All clear: Cross the road');
+} else {
+  console.log('Stay where you are.');
 }
-
-Cat.prototype = {
-  sleep: function() {
-    return 'sleeping';
-  },
-  dream: function() {
-    return 'dreaming';
-  }
-};
-
-let macy = new Cat('gray and white', 'female');
-let milo = new Cat('black and white', 'male');
-
-console.log(
-  'I am a ' + macy.color + ', ' + macy.gender + ' and I am ' + macy.sleep()
-);
-
-console.log(
-  'I am a ' + milo.color + ', ' + milo.gender + ' and I am ' + milo.dream()
-);
