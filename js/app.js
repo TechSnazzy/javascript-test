6cd0e09fffa04097216cb4f304a1e65ebdef1ac4
Sean Morrison <sean1972@gmail.com>
2019-05-19 22:11:31 -0700

    For loop decrement the array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 let classRegister = ['Lawrence', 'John', 'Jeff'];
 
+// Increment
+
 for (let i = 0; i < classRegister.length; i++) {
+  console.log(classRegister[i]);
+}
+
+// Decrement
+
+for (let i = classRegister.length - 1; i >= 0; i--) {
   console.log(classRegister[i]);
 }
