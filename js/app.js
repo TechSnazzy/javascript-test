e1a33329a44a97ad4f18b79dd7d828c2b29d292f
Sean Morrison <sean1972@gmail.com>
2019-05-21 21:56:50 -0700

    Added div container and css to center it

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 var h1 = document.getElementById('h1-style');
 var h2 = document.getElementById('h2-style');
-var body = document.getElementById('main-body');
 
 h1.style.cssText = ' background:#202020; width:300px; height: 100px; ';
 h2.style.cssText = ' background:#202020; width:300px; height: 100px; ';
-body.style.cssText = ' background:#303030; color:white; ';
