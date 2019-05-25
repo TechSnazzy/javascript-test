3549edd6259bc50536e876f74d964a83df19e23c
Sean Morrison <sean1972@gmail.com>
2019-05-24 22:50:34 -0700

    Some more examples of callback functions with a styled css button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 /*
 A function is passed into another function.
 Then after something has happened, that function is invoked.
-*/
+For example...
 
 setTimeout(function() {
   console.log('Callback function!');
 }, 3000);
+*/
+
+let btn = document.querySelector('#btn');
+
+// addEventListener takes a callback function
+btn.addEventListener('click', function(e) {
+  console.log('The button was clicked.');
+});
